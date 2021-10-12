172abe5879693ef80e5ceab0ac911be380939b61
Dex <Dani31h0@elev.techcollege.dk>
2021-10-12 11:29:58 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Kolding-Opgave
+Dokumentation
+.............
+Daniel:
+jeg har lavet forsiden undtagen nav og footer
+jeg også lavet underside 3 og 4, jeg lavede dem med testbilleder så de var klar til bare at putte det indhold/billeder på som de forskellige sider nu skulle havde for at være færdig.
+
+derefter lavede jeg 
+:Børneliv
+:Geografisk-have
+:Havnen
+:Historiske bygninger
+:Kolding som designby
+:Natteliv
+:Seværdigheder
+:Trafik
+
+og et muligt design til webshoppen med lidt javascript(for sjov) hvilket vil sige der er stadig en del fejl. 
+
+siderne er sat op med
+grid-template-area
+og position, relativ/absolut på header
+billederne der brugt er redigeret i Photoshop så de passer bedre til siderne.
+
+der mangler @media på siderne 
+
+Konklusion: Jeg har lært at være mere åben overfor at give plads til andres mening og måder at gør tingene på. jeg føler dog at det kunne være gjort på en mere produktiv måde. den måde arbejds strukturen og kommunikationen forgik på var ikke optimal.
+
+jeg kunne nævne en masse ting jeg ville havde gjort anderledes :) men alt i alt lærigt projekt men føler jeg kunne lære mere hvis der var normal undervisning.
---
+++ js/script.js
@@ -0,0 +1,57 @@
+let shop1 = document.getElementById("shop1")
+let shop2 = document.getElementById("shop2")
+let shop3 = document.getElementById("shop3")
+let shop4 = document.getElementById("shop4")
+let shop5 = document.getElementById("shop5")
+let shop6 = document.getElementById("shop6")
+let shop7 = document.getElementById("shop7")
+let shop8 = document.getElementById("shop8")
+let sum = 0
+function shopping1()
+{
+    document.getElementById("items").innerHTML = "Billeter x" + shop1.value + " :Pris pr stk: 100kr "
+    sum = sum + shop1.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping2()
+{
+    document.getElementById("items2").innerHTML = "Billeter x" + shop2.value + " :Pris pr stk: 100kr "
+    sum = sum + shop2.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping3()
+{
+    document.getElementById("items3").innerHTML = "Billeter x" + shop3.value + " :Pris pr stk: 100kr "
+    sum = sum + shop3.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping4()
+{
+    document.getElementById("items4").innerHTML = "Billeter x" + shop4.value + " :Pris pr stk: 100kr "
+    sum = sum + shop4.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping5()
+{
+    document.getElementById("items5").innerHTML = "Billeter x" + shop5.value + " :Pris pr stk: 100kr "
+    sum = sum + shop5.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping6()
+{
+    document.getElementById("items6").innerHTML = "Billeter x" + shop6.value + " :Pris pr stk: 100kr "
+    sum = sum + shop6.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping7()
+{
+    document.getElementById("items7").innerHTML = "Billeter x" + shop7.value + " :Pris pr stk: 100kr "
+    sum = sum + shop7.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
+function shopping8()
+{
+    document.getElementById("items8").innerHTML = "Billeter x" + shop8.value + " :Pris pr stk: 100kr "
+    sum = sum + shop8.value * 100;
+    document.getElementById("sum").innerHTML = "I alt købt for: " + sum + "Kr"
+}
